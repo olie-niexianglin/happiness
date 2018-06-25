bb351d7f94316ba39391a8a92c323685b6b9640e
niexl <niexianglinolie@126.com>
2018-06-25 20:31:05 +0800

    maven frame learn 2018/06/25

---
+++ olie-frame/code-maven/src/main/java/App.java
@@ -0,0 +1,7 @@
+/**
+ * @Auther: niexianglin you can mail to niexl@inspur.com
+ * @Date: 2018/6/25 12:29
+ * @Description:
+ */
+public class App {
+}
---
+++ olie-frame/code-maven/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @Auther: niexianglin you can mail to niexl@inspur.com
+ * @Date: 2018/6/25 11:23
+ * @Description:
+ */
+public class AppTest {
+    @Test
+    public void sayHello() {
+        assertTrue( true );
+    }
+}
