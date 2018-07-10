c572632c272db0d8f52f48627bc568f04a8ae650
olie <niexianglinolie@126.com>
2018-07-10 23:17:47 +0800

    maven learn 2018/7/10

---
+++ olie-treasure/src/test/java/RactorParamater.java
@@ -0,0 +1,25 @@
+import java.io.PrintStream;
+
+/**
+ * @Auther: niexianglin you can mail to niexl@inspur.com
+ * @Date: 2018/7/10 23:05
+ * @Description:
+ */
+public class RactorParamater {
+
+    /**
+     * Ctrl + Alt + P ：重构参数,
+     */
+
+    //重构之前
+    public void sayHello() {
+        System.out.printf("x");
+    }
+
+    //重构之后
+    public void sayHello(String format) {
+        System.out.printf(format);
+    }
+
+
+}
