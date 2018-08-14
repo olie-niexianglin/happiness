560fa39141a46963e4634032f10d64bc3493ac6b
niexl <niexianglinolie@126.com>
2018-08-14 19:46:34 +0800

    add @Data

---
+++ olie-treasure/idea-learn/src/main/java/olie/plugins/lombok/data/DataExample.java
@@ -0,0 +1,22 @@
+package olie.plugins.lombok.data;
+
+/**
+ * @Auther: niexianglin you can mail to niexl@inspur.com
+ * @Date: 2018/8/14 19:43
+ * @Description:
+ */
+import lombok.*;
+
+@Data public class DataExample {
+    private final String name;
+    @Setter(AccessLevel.PACKAGE) private int age;
+    private double score;
+    private String[] tags;
+
+    @ToString(includeFieldNames=true)
+    @Data(staticConstructor="of")
+    public static class Exercise<T> {
+        private final String name;
+        private final T value;
+    }
+}
