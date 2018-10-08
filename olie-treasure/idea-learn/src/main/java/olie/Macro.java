2dd56a844dbd414025914547251da4c6f89be56d
olie-niexianglin <niexianglinolie@126.com>
2018-10-08 10:35:25 +0800

    Macro 修改

--- olie-treasure/idea-learn/src/main/java/olie/Macro.java
+++ olie-treasure/idea-learn/src/main/java/olie/Macro.java
@@ -8,8 +8,9 @@ package olie;
 public class Macro {
 
     /**
-     * IntelliJ IDEA 编辑器中为 Macros 提供了一种方便的方式来自动执行您在编写代码时经常执行的重复性过程。
-     * 使用 IntelliJ IDEA 编辑器的 Macros 您可以记录、编辑和回放 Macros 等操作，为其分配快捷方式，并分享它们。
+     * IntelliJ Idea Macro:日常工作中会有一些重复性的代码录入操作。Macro 可以帮助你记录编码过程，在需要的时候可以进行
+     * 回放。
+     * 注意：Macro 只会记录你在编辑器中的录入操作，对于按钮点击，快捷键是无法有效记录的。
      */
 
     /*
